307fc5393ddb116584af329c6030800fc7d700b8
Alisonbeee <alisondianastewart@gmail.com>
2020-10-12 15:33:32 +0100

    Proff skill

--- ProffSkill R extra stuff/Mixed_Effect_Models.R
+++ ProffSkill R extra stuff/Mixed_Effect_Models.R
@@ -9,6 +9,7 @@
 install.packages("lme4")
 library(lme4)
 
+
 # This helps evalutate mixed models
 install.packages("MuMIn")
 library(MuMIn)
@@ -16,6 +17,7 @@ library(MuMIn)
 
 # adding river basin into the model greatly improves the model fit. 
 # Allowing for interaction, i.e. allowing for different river basins to differ in how phosphorous varies between upland and floodplain habitats, appears to achieve a better fit
+# log is skuqeued so we use log 
 P_model1 <- lm(log(Phosphorus)~Habitat,data=soils)
 P_model2 <- lm(log(Phosphorus)~Habitat+River_Basin,data=soils)
 P_model3 <- lm(log(Phosphorus)~Habitat*River_Basin,data=soils)
@@ -24,6 +26,7 @@ AICc(P_model1,P_model2,P_model3)
 AIC(P_model1,P_model2,P_model3)
 
 # using MuMIn package
+# REML = FALSE it means that we can compare models that we fit (if it is TRUE your models are not comparable)
 P_model4 <- lmer(log(Phosphorus)~Habitat+(1|River_Basin),data=soils,REML=FALSE)
 P_model4
 summary(P_model4)
@@ -51,6 +54,7 @@ abline(0,1)
 # plotting observed versus predicted values is one of the main ways in which we evaluate mixed effects models
 # also plot residuals versus our predictors, to ensure that there are not biases in our predictions, heteroscedasticity issues and so forth. 
 
+# the variance between habitat is dependent on river basin in model 5
 # equivalent mixed effects model, which is allowing for random intercepts and random ‘slopes’
 P_model5 <- lmer(log(Phosphorus)~Habitat+(Habitat|River_Basin),data=soils,REML=FALSE)
 AICc(P_model1,P_model4,P_model5)
@@ -66,4 +70,12 @@ r.squaredGLMM(P_model5)
 # The function gives you a marginal (R2m) and 
 # conditional (R2c) for each model. 
 # full explanation of this in word doc
+
+# difference between R squared (as a measure how strong the respose variable is)
+# and pseudo R squared (math is different) (measuring strenght of the interations?)(you need to use this one for GLMM)
+# Do not use pseudo R squared to chose between models better to use AIC, dont use because you could be over fitting
+# in picture 1 (there are just two values for fitted because there is only one predictor)
+# in picture 2 (there are are 9 levels of y axis( 5 and 4 riverbasins) x vaires with the river basin )
+
+# its ok to be a bit confused as this is complex 
 
