7a54fea224caa1001c34474976a3c6b5121b4430
Alisonbeee <alisondianastewart@gmail.com>
2020-10-14 14:49:31 +0100

    Tutorial Challenge

---
+++ Week 4/EDM_Challenge.R
@@ -0,0 +1,17 @@
+# Efficient Data Manipulation Challenge
+# 14th Oct 2020
+# Alison Stewart
+
+# Objective:
+# The Craigmillar Castle team would like a summary of the different species found 
+  # within its grounds, but broken down in four quadrants (NE, NW, SE, SW). 
+    # You can start from the trees.genus object created earlier.
+
+# Aims:
+# 1.Can you calculate the species richness (e.g. the number of different species) in each quadrant?
+# 2.They would also like to know how abundant the genus Acer is (as a % of the total number of trees) in each quadrant.
+# 3.Finally, they would like, for each quadrant separately, a bar plot showing counts of Acer trees 
+    # in the different age classes, ordered so they read from Young (lumping together juvenile and semi-mature trees), Middle Aged, and Mature.
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
